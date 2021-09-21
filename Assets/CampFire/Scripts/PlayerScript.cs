c0800491e0944d27c27c65b304df844301d8ab36
Ponce Sr <sjponce@student.uiwtx.edu>
2021-09-21 15:28:20 -0500

    Input read in MoveInput variable

--- Assets/CampFire/Scripts/PlayerScript.cs
+++ Assets/CampFire/Scripts/PlayerScript.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PlayerInputs playerInputs;
+    Vector2 MoveInput;
+    private void Awake()
     {
-        
+        playerInputs = new PlayerInputs();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        playerInputs.Enable();
+    }
+    private void OnDisable()
+    {
+        playerInputs.Disable();
+    }
+
+    private void Start()
+    {
+        playerInputs.Gameplay.Move.performed += OnMoveInputUpdated;
+        playerInputs.Gameplay.Move.canceled += OnMoveInputUpdated;
+    }
+    void OnMoveInputUpdated(InputAction.CallbackContext ctx)
+    {
+        MoveInput = ctx.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        Debug.Log($"Player Input is: {MoveInput}");
     }
 }
